ad3bbbe6e605b44be3e7a3a064bda812e377323a
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-06-21 06:54:46 +1000

    More shell.nix changes

--- shell.nix
+++ shell.nix
@@ -1,13 +1,18 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell "devika-baseline-blog" {
+pkgs.mkShell {
+  name = "devika-baseline-blog-shell";
   packages = with pkgs; [
     nodejs_22
     pnpm_9
     jre8
     jq
     awscli
+    (writeShellScriptBin "start" ''
+      ${mprocs}/bin/mprocs --npm
+    '')
   ];
   shellHook = ''
-    exec zsh
+    export SHELL=${pkgs.zsh}/bin/zsh
+    exec $SHELL
   '';
 }
