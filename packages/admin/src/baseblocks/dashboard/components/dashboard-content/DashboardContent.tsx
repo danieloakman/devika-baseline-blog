1d94d0f99898f0fa88090b5e5202bb7280464151
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-06-23 17:04:11 +1000

    Add basic blog create form to dashboard

--- packages/admin/src/baseblocks/dashboard/components/dashboard-content/DashboardContent.tsx
+++ packages/admin/src/baseblocks/dashboard/components/dashboard-content/DashboardContent.tsx
@@ -1,33 +1,97 @@
 import React from 'react';
 import styles from './DashboardContent.module.scss';
 import { getRequestHandler } from '@baseline/client-api/request-handler';
-import { useGetBlogs } from '@baseline/client-api/blogs';
-import { Spinner } from 'reactstrap';
+import { useCreateBlog, useGetBlogs } from '@baseline/client-api/blogs';
+import {
+  Spinner,
+  Input,
+  Button,
+  Toast,
+  ToastHeader,
+  ToastBody,
+} from 'reactstrap';
+import { Controller, useForm } from 'react-hook-form';
 
 const DashboardContent = (): JSX.Element => {
+  return (
+    <div className={styles.dashboard}>
+      <h1>Dashboard</h1>
+      <div className={styles.grid}>
+        <BlogCreateForm />
+        {/* <Blogs /> */}
+      </div>
+    </div>
+  );
+};
+
+const BlogCreateForm = () => {
+  const createBlog = useCreateBlog(getRequestHandler());
+  const { control, handleSubmit } = useForm<{ title: string; content: string }>(
+    {
+      defaultValues: {
+        title: '',
+        content: '',
+      },
+    },
+  );
+
+  const onSubmit = handleSubmit((data) => {
+    createBlog.mutate(data);
+  });
+
+  return (
+    <form onSubmit={onSubmit} className={styles.preview}>
+      <Controller
+        name="title"
+        control={control}
+        render={({ field }) => (
+          <label>
+            <p>Title</p>
+            <Input type="text" placeholder="Title" {...field} />
+          </label>
+        )}
+      />
+      <Controller
+        name="content"
+        control={control}
+        render={({ field }) => (
+          <label>
+            <p>Content</p>
+            <Input type="text" placeholder="Content" {...field} />
+          </label>
+        )}
+      />
+
+      <Button type="submit" color="primary">
+        Create Blog
+      </Button>
+      {createBlog.isError && (
+        <Toast>
+          <ToastHeader>Error</ToastHeader>
+          <ToastBody>
+            <pre>{JSON.stringify(createBlog.error, null, 2)}</pre>
+          </ToastBody>
+        </Toast>
+      )}
+    </form>
+  );
+};
+
+const Blogs = () => {
   const {
     data: blogs = [],
     isSuccess,
     isLoading,
     error,
   } = useGetBlogs(getRequestHandler());
-  return (
-    <div className={styles.dashboard}>
-      <h1>Dashboard</h1>
-      <div className={styles.grid}>
-        <div className={styles.preview}>
-          {isLoading ? (
-            <Spinner color="primary" />
-          ) : isSuccess ? (
-            blogs.map((blog) => (
-              <pre key={blog.id}>{JSON.stringify(blog, null, 2)}</pre>
-            ))
-          ) : (
-            <pre>Error: {JSON.stringify(error, null, 2)}</pre>
-          )}
-        </div>
-      </div>
-    </div>
+  return isLoading ? (
+    <Spinner color="primary" />
+  ) : isSuccess ? (
+    blogs.map((blog) => (
+      <pre key={blog.id}>{JSON.stringify(blog, null, 2)}</pre>
+    ))
+  ) : (
+    <pre>Error: {JSON.stringify(error, null, 2)}</pre>
   );
 };
 
