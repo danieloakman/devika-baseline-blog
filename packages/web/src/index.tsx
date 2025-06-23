cc57ed771ad66bfff41f6b2427abe162f2de300a
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-06-23 19:44:24 +0000

    packages/web now use shared styles

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './index.scss';
-import './styles/fonts.scss';
+import '@baseline/components/styles/fonts';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
