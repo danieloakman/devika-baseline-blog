9c7b407759ec54ef5450ab1b2b7d8d088e5e2feb
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-06-24 18:32:48 +0000

    Add dashboard title

--- packages/admin/src/baseblocks/dashboard/pages/Dashboard.tsx
+++ packages/admin/src/baseblocks/dashboard/pages/Dashboard.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import PageContent from '../../../components/page-content/PageContent';
 import DashboardContent from '../components/dashboard-content/DashboardContent';
+import { Helmet } from 'react-helmet';
 
 const Dashboard = (): JSX.Element => (
   <PageContent>
+    <Helmet>
+      <title>Dashboard</title>
+    </Helmet>
     <DashboardContent />
   </PageContent>
 );
